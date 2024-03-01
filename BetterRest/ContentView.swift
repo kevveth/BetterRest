e3bb933c4be2f978d6495de4fe62c42527862be2
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-02-29 17:09:54 -0800

    Connected SwiftUI to CoreML

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -5,13 +5,19 @@
 //  Created by Kenneth Oliver Rathbun on 2/22/24.
 //
 
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 6.0
     @State private var coffeeAmount = 1
-
+    
+    // Alert displaying the time the user should go to sleep
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -23,20 +29,52 @@ struct ContentView: View {
                 Text("Desired amount of sleep")
                     .font(.headline)
                 Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.5)
+                    .padding(.horizontal)
                 
                 Text("Daily coffee intake")
                     .font(.headline)
                 Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20, step: 1)
+                    .padding(.horizontal)
             }
             .navigationTitle("Better Rest")
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
     
     func calculateBedtime() {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            // Get date components for the wake up time and convert them to seconds
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            
+            // Feed values into CoreML and see what comes out
+            let prediction = try model.prediction(wake: Int64(hour + minute), estimatedSleep: sleepAmount, coffee: Int64(coffeeAmount))
+            
+            // Sleep time is the wake up time minus the amount of time required to sleep
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime is..."
+//            alertMessage = "\(sleepTime)"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        }
+        catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
         
+        showingAlert = true
     }
 }
 
